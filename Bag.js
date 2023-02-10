3b3326f635d84e62c18a86b4c0550f11aecf7e73
anis913 <anisislam913@gmail.com>
2023-02-10 00:54:43 +0000

    changed classes

--- Bag.js
+++ Bag.js
@@ -1,8 +1,14 @@
 class Bag {
-    // Include constructor Method with the following property: 
-        // 1. Weight
-            //1a. Should handle an exception in case user adds no weight and throw an error.
-  
+    constructor(weight) {
+        if (!weight) {
+          throw new Error("bag needs weight");
+        }
+        this.weight = weight;
+      }
+
+    // Include constructor Method with the following property:  
+    
 }
 
 //Export the module
+module.exports = Bag;
--- Person.js
+++ Person.js
@@ -1,10 +1,17 @@
 class Person {
+    constructor(name) {
+        this.name = name;
+        this.bags = [];
+      }
+    
+      addBag(bag) {
+        this.bags.push(bag);
+      }
+      
     // Include a Constructor Method for Properties: 
-        // 1. Name
-        // 2. Bags
-            // 2a. Bags should contain/reference a data type for storing more than one bag object.
 
     // Include an addBag method
 }
 
 //Export the Module
+module.exports = Person;
